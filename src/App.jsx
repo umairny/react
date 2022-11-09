a8902ce00fd2088b48afa35c9edbf0a4302ae2a1
umairny <umairny1@gmail.com>
2022-11-09 11:13:52 -0500

    update menu to reponsive

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ function App() {
           <Route exact path='/tenzies' element={<Tenzies />} />
           <Route exact path='/notes' element={<Notes />} />
         </Routes>
-          
       </HashRouter>
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,35 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import './Header.css'
 
 export default function Header () {
+	const [toggle, setToggle] = useState(false)
+	//console.log(toggle)
+	const location = useLocation()
+	//console.log(location.pathname)
+	function animate(x){
+		x.classList.toggle("animate")
+	}
     return(
-    <header>
-		<nav>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/quiz">Quizlet</Link>
-				</li>
-				<li>
-					<Link to="/tenzies">Tenzies</Link>
-				</li>
-				<li>
-					<Link to="/notes">Notes</Link>
-				</li>
-			</ul>
+		<nav className='navbar'>
+			<div className='nav-cont'>
+				<div onClick={() => setToggle(false)} className='nav-logo'>
+					<Link className={location.pathname === "/" ? "active" : ""} to="/">Projects</Link>
+				</div>
+				<div className={toggle === true ? 'menu show' : 'menu hide'}>
+					<Link onClick={() => setToggle(false)} className={location.pathname === "/quiz" ? "active" : ""} to="/quiz">Quizlet</Link>
+					<Link onClick={() => setToggle(false)} className={location.pathname === "/tenzies" ? "active" : ""} to="/tenzies">Tenzies</Link>
+					<Link onClick={() => setToggle(false)} className={location.pathname === "/notes" ? "active" : ""} to="/notes">Notes</Link>
+				</div>
+				<div onClick={() => setToggle(!toggle)} className={toggle === true ? 'toggle-btn animate' : 'toggle-btn'}>
+					<div className='bar1'></div>
+					<div className='bar2'></div>
+					<div className='bar3'></div>
+				</div>
+
+
+			</div>
 		</nav>
-	</header>
-
-
     )
 }
 
